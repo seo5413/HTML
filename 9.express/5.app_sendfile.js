0463f2cb8e48b139d4ab8655c25e320ffa18417e
seo5413 <seo5413@naver.com>
2025-04-15 10:30:27 +0900

    sendfiel

--- 9.express/5.app_sendfile.js
+++ 9.express/5.app_sendfile.js
@@ -3,10 +3,14 @@ const path = require('path');
 const app=express();
 const port=3000;
 
+app.use(express.static('public'));                              //우리의 홈에있는 public 폴더를 정적 폴더로 정의함
+
 app.get('/', (req,res) =>{
-    const htmlFilePath = path.join(__dirname, 'index.html');    //절대경로(absolute path, full path)
+    const htmlFilePath = path.join(__dirname,'public', 'index.html');    //절대경로(absolute path, full path)
     res.send(htmlFilePath);
 });
+
+
 
 app.listen(port, () =>{
     console.log('서버 레디')
